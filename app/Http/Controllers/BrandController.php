438c5503ce48fe2404acf799c5f8dc9630caec15
kayalraju <87748199+kayalraju@users.noreply.github.com>
2022-02-08 13:57:31 +0530

    banner

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -18,7 +18,7 @@ class BrandController extends Controller
     public function StoreBrand(Request $request){
         $validatedData = $request->validate([
             'brand_name' => 'required|unique:brands|min:4',
-            'brand_image' => 'required',
+            'brand_image' => 'required|mimes:jpg.jpeg,png',
             
         ],
         [
@@ -26,18 +26,18 @@ class BrandController extends Controller
             'brand_image.min' => 'Brand Longer then 4 Characters', 
         ]);
 
-        $brand_image =  $request->file('brand_image');
-        $name_gen = hexdec(uniqid());
-        $img_ext = strtolower($brand_image->getClientOriginalExtension());
-        $img_name = $name_gen.'.'.$img_ext;
-        $up_location = 'image/brand/';
-        $last_img = $up_location.$img_name;
-        $brand_image->move($up_location,$img_name);
+        // $brand_image =  $request->file('brand_image');
+        // $name_gen = hexdec(uniqid());
+        // $img_ext = strtolower($brand_image->getClientOriginalExtension());
+        // $img_name = $name_gen.'.'.$img_ext;
+        // $up_location = 'image/brand/';
+        // $last_img = $up_location.$img_name;
+        // $brand_image->move($up_location,$img_name);
 
-        //$name_gen = hexdec(uniqid()).'.'.$brand_image->getClientOriginalExtension();
-        //Image::make($brand_image)->resize(300,200)->save('image/brand/'.$name_gen);
+        $name_gen = hexdec(uniqid()).'.'.$brand_image->getClientOriginalExtension();
+        Image::make($brand_image)->resize(300,200)->save('image/brand/'.$name_gen);
 
-        //$last_img = 'image/brand/'.$name_gen;
+        $last_img = 'image/brand/'.$name_gen;
  
         Brand::insert([
             'brand_name' => $request->brand_name,
@@ -45,12 +45,12 @@ class BrandController extends Controller
             'created_at' => Carbon::now()
         ]);
          
-        $notification = array(
-            'message' => 'Brand Inserted Successfully',
-            'alert-type' => 'success'
-        );
+        // $notification = array(
+        //     'message' => 'Brand Inserted Successfully',
+        //     'alert-type' => 'success'
+        // );
 
-        return Redirect()->back()->with($notification);
+        return Redirect()->back()->with('success','brand successfully');
 
     }
 
@@ -59,4 +59,70 @@ class BrandController extends Controller
         return view('admin.brand.edit',compact('brands'));
 
     }
+     public function Update(Request $request, $id){
+
+        $validatedData = $request->validate([
+            'brand_name' => 'required|min:4',
+                       
+        ],
+        [
+            'brand_name.required' => 'Please Input Brand Name',
+            'brand_image.min' => 'Brand Longer then 4 Characters', 
+        ]);
+
+        $old_image = $request->old_image;
+
+        $brand_image =  $request->file('brand_image');
+
+        if($brand_image){
+        
+        $name_gen = hexdec(uniqid());
+        $img_ext = strtolower($brand_image->getClientOriginalExtension());
+        $img_name = $name_gen.'.'.$img_ext;
+        $up_location = 'image/brand/';
+        $last_img = $up_location.$img_name;
+        $brand_image->move($up_location,$img_name);
+
+        unlink($old_image);
+        Brand::find($id)->update([
+            'brand_name' => $request->brand_name,
+            'brand_image' => $last_img,
+            'created_at' => Carbon::now()
+        ]);
+
+        // $notification = array(
+        //     'message' => 'Brand Updated Successfully',
+        //     'alert-type' => 'info'
+        // );         
+        return Redirect()->back()->with('success','brand update successfully');
+
+        }else{
+            Brand::find($id)->update([
+                'brand_name' => $request->brand_name,
+                'created_at' => Carbon::now()
+            ]);
+            $notification = array(
+                'message' => 'Brand Updated Successfully',
+                'alert-type' => 'warning'
+            );    
+             
+            return Redirect()->back()->with($notification);
+
+        } 
+    }
+
+    public function Delete($id){
+         $image = Brand::find($id);
+         $old_image = $image->brand_image;
+         unlink($old_image);
+
+        Brand::find($id)->delete();
+        // $notification = array(
+        //     'message' => 'Brand Delete Successfully',
+        //     'alert-type' => 'error'
+        // );   
+        return Redirect()->back()->with('success','delete successfully');
+
+     }
+
 }
--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -2,62 +2,56 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
            
-           All Category<b>  </b>
+           Edit Brand<b>  </b>
           
 
         </h2>
     </x-slot>
 
     <div class="py-12"> 
-   
-          <div class="card-header"> All Brand </div>
-    
+   <div class="container">
+    <div class="row">
 
-    
-    <div class="col-md-4">
+    <div class="col-md-8">
      <div class="card">
-          <div class="card-header"> update Brand </div>
+          <div class="card-header"> Edit brand </div>
           <div class="card-body">
-          
-         
-         
-          <form action="{{ route('store.brand') }}" method="POST" enctype="multipart/form-data">
+  <form action="{{ url('brand/update/'.$brands->id)  }}" method="POST" enctype="multipart/form-data">
           @csrf
+  <input type="hidden" name="old_image" value="{{ $brands->brand_image }}">
   <div class="form-group">
-    <label for="exampleInputEmail1">update Brand name</label>
-    <input type="text" name="brand_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="exampleInputEmail1">Update Brand Name</label>
+    <input type="text" name="category_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ $brands->brand_name }}">
 
-          @error('brand_name')
+          @error('category_name')
                <span class="text-danger"> {{ $message }}</span>
           @enderror
+
   </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Update Brand image </label>
+    <input type="file" name="brand_image" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ $brands->brand_image }}">
 
-   <div class="form-group">
-    <label for="exampleInputEmail1">update Brand Image</label>
-    <input type="file" name="brand_image" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-
-          @error('brand_image')
+          @error('category_name')
                <span class="text-danger"> {{ $message }}</span>
           @enderror
+
   </div>
+  <div class="form-group">
+     <img src="{{ asset($brands->brand_image) }}" style="width:400px; height:200px;" >
+
+     </div>
      
-  <button type="submit" class="btn btn-primary">update Brand</button>
+  <button type="submit" class="btn btn-primary">Update brand</button>
 </form>
+
        </div>
 
     </div>
+  </div>
+   
+ </div>
   </div> 
-    
-
-
-
-   <!-- Trash Part -->
-
-  
-
-   <!-- End Trush -->
-
-
 
     </div>
 </x-app-layout>
